0507ed30e75b036aa5d7a064f7ea16848ede4001
Max <thepro545@mail.ru>
2022-07-17 13:36:28 +0300

    Initial commit

---
+++ src/main/java/pro/sky/GroupWorkJava/GroupWorkJavaApplication.java
@@ -0,0 +1,12 @@
+package pro.sky.GroupWorkJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupWorkJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupWorkJavaApplication.class, args);
+	}
+}
---
+++ src/test/java/pro/sky/GroupWorkJava/GroupWorkJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.GroupWorkJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupWorkJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
